dfedff53268a2a368897cb5becb46587ada1ca22
nishi1305 <gnishi133@gmail.com>
2023-01-08 21:41:29 +0530

    commit

--- project4/JS files/script.js
+++ project4/JS files/script.js
@@ -2,15 +2,274 @@
 let hover = document.querySelector('.lists_icon');
 let submenu = document.querySelector('.ellipsis_list');
 
-hover.addEventListener('click',()=>{
+hover.addEventListener('click', () => {
    submenu.style.display = "block";
 });
 
-hover.addEventListener('dblclick',()=>{
-    submenu.style.display = "none";
- });
- 
+hover.addEventListener('dblclick', () => {
+   submenu.style.display = "none";
+});
 
-let liked = document.getElementById(liked);
-let liked_page = liked.link("http://127.0.0.1:5500/PROJECTS/project4/liked.html");
 
+
+let title = document.querySelector('.title');
+let album = document.querySelector('.album');
+let prev = document.querySelector('.prev');
+let playPause = document.querySelector('#playPause');
+let next = document.querySelector('.next');
+let audio = document.querySelector('audio');
+let img = document.querySelector('.Song_cover');
+
+
+
+//songlist
+
+let songList = [
+   {
+      path:     "./songs/s1.mp3",
+      songName: "Perfect",
+      songAlbum:"Ed sheeran",
+      cover:"./images/1.jpg"
+
+   },
+   {
+      path:     "./songs/s2.mp3",
+      songName: "Breathing",
+      songAlbum:"Therapy",
+      cover:"./images/m2.jpg"
+
+
+   },
+   {
+      path:     "./songs/s3.mp3",
+      songName: "Closer",
+      songAlbum:"The Chainsmoker-Helsay ",
+      cover:"./images/m3.jpg"
+
+
+   },
+   {
+      path:     "./songs/s4.mp3",
+      songName: "Mehmaan",
+      songAlbum:"Mismatched",
+      cover:"./images/m4.jpg"
+
+
+   },
+   {
+      path:     "./songs/s5.mp3",
+      songName: "Kahani",
+      songAlbum:"Pritam-(From-Laal Singh)",
+      cover:"./images/m5.jpg"
+
+
+   },
+   {
+      path:     "./songs/s6.mp3",
+      songName: "You Broke me first ",
+      songAlbum:"Tata McRae",
+      cover:"./images/m6.jpg"
+
+
+   },
+   {
+      path:     "./songs/s7.mp3",
+      songName: "Without Me",
+      songAlbum:"Manic",
+      cover:"./images/m7.jpg"
+
+
+   },
+   {
+      path:     "./songs/s8.mp3",
+      songName: "Kitni Haseen Hogi",
+      songAlbum:"Arijit Singh",
+      cover:"./images/m8.jpg"
+
+
+   },
+   {
+      path:     "./songs/s9.mp3",
+      songName: "Soch liya",
+      songAlbum:"Radhe Shyam",
+      cover:"./images/m9.jpg"
+
+
+   },
+   {
+      path:     "./songs/s10.mp3",
+      songName: "What If",
+      songAlbum:"Jhhnny Orlando",
+      cover:"./images/m10.jpg"
+      
+
+   },
+   {
+      path:     "./songs/s11.mp3",
+      songName: "Rang Lageya",
+      songAlbum:"Mohit Chauhan",
+      cover:"./images/m11.jpg"
+
+
+   }
+
+
+];
+
+
+
+let song_Playing = false;
+
+//Play song//
+
+function playSong(){
+   song_Playing = true;
+   audio.play();
+   playPause.classList.add('active');
+   //change icon
+
+   // playPause.innerHTML ="<i class="fa-solid fa-circle-pause"></i>";
+}
+
+//Pause song//
+
+function pauseSong(){
+   song_Playing = false;
+   audio.pause();
+   playPause.classList.remove('active');
+
+   // playPause.innerHTML ="<i class="fa-solid fa-circle-play"></i>";
+
+}
+
+
+//play or pause song on click//
+
+playPause.addEventListener("click", () => (song_Playing ?
+   pauseSong() : playSong()));
+
+
+
+//load song//
+
+
+function loadSong(songList){
+   title.textContent = songList.songName;
+   album.textContent = songList.songAlbum;
+   audio.src = songList.path;
+   img.src = songList.cover;
+}
+
+
+//current song
+
+
+let i = 0;
+
+
+
+//on load -select first song from song list
+
+
+loadSong(songList[i]);
+
+//previous song
+
+function prevSong(){
+   i--;
+   if (i<0){
+      i = songList.length -1;
+
+   }
+   loadSong(songList[i]);
+   playSong();
+}
+prev.addEventListener("click",prevSong);
+
+
+//Next song
+
+function nextSong(){
+   i++;
+   if (i > songList.length - 1){
+      i = 0;
+
+   }
+   loadSong(songList[i]);
+   playSong();
+}
+next.addEventListener("click",prevSong);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".my_songs").trigger('load');
+// function play_audio(task){
+//    if (task == 'play'){
+//       $(".mysongs").trigger('play');
+//    }
+//    if (task =='stop'){
+//       $(".my_songs").trigger(pause);
+//       $(".my_songs").prop("currentTime",0);
+
+//    }
+// }
+
+
+// //load
+
+// keys = Object.keys(playlist);
+// $('.my_songs').append("<source id='sound_src' src=" + playlist[keys[0]]  + "type='audio/mpeg'>");
+
+
+
+// //reset
+
+// count = 0;
+// $('my_songs').on('ended', function(){
+//    count++;
+//    $("#sound_src").attr("src", playlist[keys[count]])[0];
+//    $(".my_audio").trigger('load');
+//    play_audio('play');
+// })
